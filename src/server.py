9d51a6dca6b5aca33aaa688e9f73ca8f91a25ceb
Nacho Gonzalez <nachogonz2702@gmail.com>
2023-11-30 00:55:10 -0300

    Added partial clock checking for rollback and tie-breaking in case of simultaneous broadcasts

--- src/server.py
+++ src/server.py
@@ -14,6 +14,8 @@ me = (sys.argv[1], sys.argv[2])
 server_nodes = set()
 active_nodes = set()
 content = ""
+last_operation = ()  # [0] boolean: broadcast done, [1] Operation operation, [2] int: pos, [3] string: char
+rollback_operation = ()  # [0] Operation operation, [1] int: pos, [2] string: char
 clock = 0
 
 
@@ -31,7 +33,9 @@ signal.signal(signal.SIGINT, signal_handler)
 
 def broadcast(request):
     global clock
+    global last_operation
     status = 0
+    last_operation[0] = True  # this is not correct, any specific peer might not have received the broadcast yet
     for (ip, port) in active_nodes - {me}:  # broadcasting the cmd
         print(f"broadcasting to: {ip}:{port}")
         with grpc.insecure_channel(f"{ip}:{port}") as channel:
@@ -45,6 +49,7 @@ def broadcast(request):
 
 def apply(operation, pos, elem):
     global content
+    global last_operation
     if pos < 0 or pos > len(content):
         return 1
 
@@ -54,21 +59,53 @@ def apply(operation, pos, elem):
         assert operation == DEL
         if pos == len(content):  # We cant delete at len(content) but we can insert there.
             return 1
+        elem = content[pos]
         content = content[:pos] + content[pos + 1:]
+
+    last_operation = (False, operation, pos, elem)
     return 0
+
+
+def rollback_required(request_port, self_port):
+    global last_operation
+    if last_operation[0]:
+        return self_port > request_port
+    else:
+        return True
+
+
+def do_rollback():
+    global last_operation
+    global rollback_operation
+    operation = DEL if last_operation[1] == INS else INS
+    pos = last_operation[2]
+    char = last_operation[3]
+    rollback_operation = (last_operation[1], pos, char)
+    apply(operation, pos, char)
 
 
 class Editor(editor_pb2_grpc.EditorServicer):
 
     def SendCommand(self, request, context):
         global clock
+        global last_operation
+        status = 0
         clock += 1
+        if request.transmitter == SERVER and request.clock < clock:  # technically clock == request.clock + 1 when conflict
+            rollback = rollback_required(request.port, me[1])
+            if rollback:
+                do_rollback()
+                status += apply(request.operation, request.position, request.char)
+                status += apply(rollback_operation[0], rollback_operation[1], rollback_operation[2])
+            else:
+                status += apply(request.operation, request.position, request.char)
+            return editor_pb2.CommandStatus(status=status)
+
         if request.operation == 0:
             print(f"receiving command: ins('{request.char}', {request.position})")
         else:
             print(f"receiving command: del({request.position})")
         print(f"from: user {request.user_id} through {'the app' if request.transmitter == 0 else 'a node'}")
-        status = 0
         status += apply(request.operation, request.position, request.char)
         clock += 1
         print(f"Content: {content}")
