0a7507d392bba860984502ab32995190ff3555d2
Nacho Gonzalez <nachogonz2702@gmail.com>
2023-12-06 14:17:04 -0300

    Partial implementation of server using logger

--- src/server.py
+++ src/server.py
@@ -14,8 +14,8 @@ me = (sys.argv[1], sys.argv[2])
 server_nodes = set()
 active_nodes = set()
 content = ""
-last_operation = ()  # [0] boolean: broadcast done, [1] Operation operation, [2] int: pos, [3] string: char
-rollback_operation = ()  # [0] Operation operation, [1] int: pos, [2] string: char
+last_operations = Logger()
+corrected_operations = [()]  # [0] boolean: broadcast done, [1] Operation operation, [2] int: pos, [3] string: char, [4] int: clock
 clock = 0
 
 
@@ -30,11 +30,9 @@ def signal_handler(sig, frame):
 signal.signal(signal.SIGINT, signal_handler)
 
 
-def broadcast(request):
-    global clock
-    global last_operation
+def broadcast(request, local_clock, log_id):
     status = 0
-    last_operation = (True, last_operation[1], last_operation[2], last_operation[3])
+    last_operations.setTrue(log_id)
     # this is not correct, any specific peer might not have received the broadcast yet
     for (ip, port) in active_nodes - {me}:  # broadcasting the cmd
         # print(f"broadcasting to: {ip}:{port}")
@@ -42,14 +40,14 @@ def broadcast(request):
             stub = editor_pb2_grpc.EditorStub(channel)
             response = stub.SendCommand(
                 editor_pb2.Command(operation=request.operation, position=request.position,
-                                   id=int(me[1]), transmitter=SERVER, char=request.char, clock=clock))
+                                   id=int(me[1]), transmitter=SERVER, char=request.char, clock=local_clock))
             status += response.status
     return status
 
 
-def apply(operation, pos, elem):
+def apply(operation, pos, elem, local_clock):
     global content
-    global last_operation
+    global last_operations
     if pos < 0 or pos > len(content):
         return 1
 
@@ -62,12 +60,13 @@ def apply(operation, pos, elem):
         elem = content[pos]
         content = content[:pos] + content[pos + 1:]
 
-    last_operation = (False, operation, pos, elem)
-    return 0
+    log_id = last_operations.log((False, operation, pos, elem, local_clock))
+    return 0, log_id
 
 
 def rollback_required(request_port, self_port):
-    global last_operation
+    global last_operations
+    last_operation = last_operations.get_last()
     if not last_operation[0]:
         return True
     print(f"this {self_port} - other {request_port}")
@@ -78,27 +77,42 @@ def rollback_required(request_port, self_port):
     return self_port > request_port
 
 
-def do_rollback():
-    global last_operation
-    global rollback_operation
-    operation = DEL if last_operation[1] == INS else INS
-    pos = last_operation[2]
-    char = last_operation[3]
-    rollback_operation = (last_operation[1], pos, char)
-    apply(operation, pos, char)
+def do_rollback(local_clock):
+    global last_operations
+    global corrected_operations
+    operations = last_operations.get_operations_out_of_time(local_clock)
+    inverse_operations = [()]
+    for operation in operations:
+        op = DEL if operation[1] == INS else INS
+        pos = operation[2]
+        char = operation[3]
+        inverse_operations.append((op, pos, char))
+        corrected_operations.append(operation)
+
+    inverse_operations.reverse()
+    for inverse_operation in inverse_operations:
+        apply(inverse_operation[0], inverse_operation[1], inverse_operation[2], local_clock)
 
 
-def handle_server_request(request):
-    global clock
-    if request.clock < clock:  # conflict
+def apply_rollback_operations():
+    global corrected_operations
+    status = 0
+    for operation in corrected_operations:
+        status += apply(operation[1], operation[2], operation[3], operation[4])[0]
+    corrected_operations.clear()
+    return status
+
+
+def handle_server_request(request, local_clock):
+    if request.clock < local_clock:  # conflict
         ip, port = me
         my_id = int(port)
         if rollback_required(my_id, request.id):
-            do_rollback()
-            status = apply(request.operation, request.position, request.char)
-            status += apply(rollback_operation[0], rollback_operation[1], rollback_operation[2])
+            do_rollback(local_clock)
+            status = apply(request.operation, request.position, request.char, request.clock)[0]
+            status += apply_rollback_operations()
         else:
-            status = apply(request.operation, request.position, request.char)
+            status = apply(request.operation, request.position, request.char, request.clock)[0]
         # print(f"Rollback Clock: {clock}")
     else:  # normal broadcast
         # if request.operation == 0:
@@ -106,27 +120,28 @@ def handle_server_request(request):
         # else:
         # print(f"receiving command: del({request.position})")
         # print(f"from: server {request.id}")
-        status = apply(request.operation, request.position, request.char)
+        status = apply(request.operation, request.position, request.char, request.clock)[0]
 
     print(f"Content: {content}")
     return status
 
 
-def handle_user_request(request):
+def handle_user_request(request, local_clock):
     global clock
     # if request.operation == 0:
     #   print(f"receiving command: ins('{request.char}', {request.position})")
     # else:
     #   print(f"receiving command: del({request.position})")
     # print(f"from: user {request.id} through the app")
-    status = apply(request.operation, request.position, request.char)
+    status, log_id = apply(request.operation, request.position, request.char, local_clock)
     print(f"Content: {content}")
 
     if status == 0:
         clock += 1
-        # print(f"increasing clock to: {clock} before broadcast")
+        local_clock += 1
+        print(f"increasing clock to: {local_clock} before broadcast")
         time.sleep(3)
-        status = broadcast(request)
+        status = broadcast(request, local_clock, log_id)
 
     return status
 
@@ -136,12 +151,13 @@ class Editor(editor_pb2_grpc.EditorServicer):
     def SendCommand(self, request, context):
         global clock
         clock = max(clock + 1, request.clock)
+        local_clock = clock
         # print(f"increasing clock to: {clock} after receiving")
-        # print(f"msg clock: {request.clock} - curr clock: {clock} - transmitter: {request.transmitter}")
+        print(f"msg clock: {request.clock} - curr clock: {local_clock} - transmitter: {request.transmitter}")
         if request.transmitter == SERVER:
-            status = handle_server_request(request)
+            status = handle_server_request(request, local_clock)
         elif request.transmitter == USER:
-            status = handle_user_request(request)
+            status = handle_user_request(request, local_clock)
         else:
             print(f"Couldn't handle request, unknown transmitter: {request.transmitter}")
             status = 1
@@ -149,7 +165,6 @@ class Editor(editor_pb2_grpc.EditorServicer):
         if status != 0:
             print("error, status: " + str(status))
         return editor_pb2.CommandStatus(status=status)
-
 
     def Notify(self, request, context):
         node = (request.ip, request.port)
