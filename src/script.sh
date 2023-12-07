62ebc13cbfed4d24f57f9e0abe5b4e9ca2397a8e
Nacho Gonzalez <nachogonz2702@gmail.com>
2023-12-07 18:35:09 -0300

    Changed script.sh to work on linux

--- src/script.sh
+++ src/script.sh
@@ -1,7 +1,7 @@
 port=$1
 if [ $port -eq 5001 ] 
 then
-    echo 'ins\n0\na\nins\n1\nb\nins\n2\nc\nexit' | python3 app.py localhost $port > /dev/null
+    echo -e 'ins\n0\na\nins\n1\nb\nins\n2\nc\nexit' | python3 app.py localhost $port
 else
-    echo 'ins\n0\nd\nins\n1\ne\nins\n2\nf\nexit' | python3 app.py localhost $port > /dev/null
+    echo -e 'ins\n0\nd\nins\n1\ne\nins\n2\nf\nexit' | python3 app.py localhost $port
 fi
