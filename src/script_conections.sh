c57b20d30069d5e12218e48a0d7418d445ec5804
Nacho Gonzalez <nachogonz2702@gmail.com>
2023-12-17 14:03:26 -0300

    Changed conections script

--- src/script_conections.sh
+++ src/script_conections.sh
@@ -1,1 +1,4 @@
-./script.sh 5001 & ./script.sh 5002 & python3 server.py localhost 5003 & ./script.sh 5004 & ./script.sh 5005 & ./script.sh 5003 &
+#!/bin/bash
+./script.sh 5001 & ./script.sh 5002 & python3 server.py localhost 5003 & ./script.sh 5004 & ./script.sh 5005 &
+sleep 0.5
+./script.sh 5003 &
